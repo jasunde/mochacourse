b47df146ca2ef72041390e060b46ebee5bbd0695
jasunde <jlaurits@gmail.com>
2015-04-01 16:00:29 -0500

    Initial commit

---
+++ test/hex2rgb.test.js
@@ -0,0 +1,25 @@
+var hex2rgb = require('../lib/hex2rgb');
+var expect = require("chai").expect;
+
+describe.only('hex2rgb', function() {
+
+	it('should return an error if value is not a hex code', function(done) {
+
+		hex2rgb('blue', function (error, result) {
+			expect(error).to.exist;
+			done();
+		});
+
+	});
+
+	it('should return a correctly converted rgb value', function(done) {
+
+		hex2rgb('#fff', function (error, result) {
+			expect(error).to.not.exist;
+			expect(result).to.deep.equal([255, 255, 255]);
+			done();
+
+		});
+
+	});
+});
